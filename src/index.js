eef4a7ff0c1a7bb3c4ee0a062f2c3ca0027117e7
ranu <lera@aluno.ifnmg.edu.br>
2025-03-06 19:37:14 +0000

    ss

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ app.get("/", (req, res) => {
   console.log("Rota / solicitada");
   // Cria a rota da raiz do projeto
   res.json({
-    nome: "Arthur Porto", // Substitua pelo seu nome
+    nome: "Ranunis", // Substitua pelo seu nome
   });
 });
 
